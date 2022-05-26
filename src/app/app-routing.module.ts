0c11d28ecc06a456ba6dae7984e9769ffc1cf0d9
Iancaov06 <iancaov06.uni@gmail.com>
2022-05-26 00:53:57 -0500

    Repositorio de Inventarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 import { CategoriaComponent } from './categoria/categoria.component';
 
 const routes: Routes = [
-  {path:'categorias', component:CategoriaComponent}
+  {path:'categorias', component:CategoriaComponent},
+  {path:'', component:BienvenidoComponent}
 ];
 
 @NgModule({
